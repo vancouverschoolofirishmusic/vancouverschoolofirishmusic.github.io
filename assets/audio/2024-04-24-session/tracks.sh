910c0a505b12be095a46e763c81f4ca7d96f6011
conorfleming <conor.fleming@gmail.com>
2024-04-30 23:07:16 -0700

    updated tracks

--- assets/audio/2024-04-24-session/tracks.sh
+++ assets/audio/2024-04-24-session/tracks.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-template="{% include embed-audio.html src="/assets/audio/2024-04-24-session/XXX" %}"
+template="{% include embed-audio.html src='/assets/audio/2024-04-24-session/XXX' %}"
 
 for track in *mp3;
 do
-	echo $track;
+	echo "\n${track%%.*} \n" >> tracks.md
 	echo $template | sed "s/XXX/${track}/g" >> tracks.md;
 done
 
